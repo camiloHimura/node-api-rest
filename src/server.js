b388ac7f84a533870f597f361b4849bc7271d76f
t_coloc <camilo.colorado@CO-IT005544.local>
2019-04-19 23:06:03 -0500

    express and nodemon added

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParse = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>{
+    res.send({message: "Hi I'm get"})
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    res.send({message: "Hi I'm post with nodemon custom config"})
+})
+
+module.exports = {
+        start(){
+            app.listen(PORT, () => console.log(`I'm node listen port ${PORT}`))
+        }
+};
